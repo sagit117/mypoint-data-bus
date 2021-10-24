91031b572726cc0b7015aaf9a38c761791e9c837
Павел Аксенов <sagit117@gmail.com>
2021-10-24 14:07:56 +0300

    добавлена точка входа для переброски запроса к БД сервису

--- src/ru/mypoint/databus/webserver/routes/Webserver.kt
+++ src/ru/mypoint/databus/webserver/routes/Webserver.kt
@@ -1,9 +1,11 @@
-package ru.mypoint.databus.routes
+package ru.mypoint.databus.webserver.routes
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import ru.mypoint.databus.webserver.dto.RequestWebServer
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.webServerModule() {
@@ -11,6 +13,12 @@ fun Application.webServerModule() {
         route("/webserver") {
             get("/ping") {
                 call.respond(HttpStatusCode.OK, "OK")
+            }
+
+            post("/dbservice/request") {
+                val request = call.receive<RequestWebServer>()
+
+                println(request.toString())
             }
         }
     }
